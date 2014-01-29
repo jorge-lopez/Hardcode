b8176a256baeb540b5255624e80b5251327ca4f8
Jorge Lopez <jorge.lopez.18820@gmail.com>
2014-01-29 15:44:50 -0800

    Primera version de la pagina

---
+++ Harcode Solutions/Pagina Principal Jorge/script.js
@@ -0,0 +1,15 @@
+var $window = $(window); 
+var velocity = 0.4; 
+
+function update()
+  { 
+    var pos = $window.scrollTop(); 
+    $('.container').each(function() 
+      { 
+        var $element = $(this); 
+        var height = $element.height(); 
+        $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
+      }); 
+  }; 
+
+$window.bind('scroll', update);
