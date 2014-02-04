bb4f7aa05eb29aa38538ac2b238ae158bce8aefe
Ivan Sanchez <ivan.rivas@cetys.edu.mx>
2014-02-03 21:24:57 -0800

    Pagina 1.512

--- Harcode Solutions/Pagina Principal/JS/script.js
+++ Harcode Solutions/Pagina Principal/JS/script.js
@@ -1,19 +1,42 @@
 
-    var $window = $(window); 
-    var velocity = 0.4; 
+    // var $window = $(window); 
+    // var velocity = 0.4; 
 
-    function update()
-    { 
-      var pos = $window.scrollTop(); 
-      $('.container').each(function() 
-        { 
-          var $element = $(this); 
-          var height = $element.height(); 
-          $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
-        }); 
-    }; 
-    $window.bind('scroll', update);
+    // function update()
+    // { 
+    //   var pos = $window.scrollTop(); 
+    //   $('.container').each(function() 
+    //     { 
+    //       var $element = $(this); 
+    //       var height = $element.height(); 
+    //       $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
+    //     }); 
+    // }; 
+    // $window.bind('scroll', update);
+$(document).ready(function(){
+  // Cache the Window object
+  $window = $(window);
+                
+   $('section[data-type="background"]').each(function(){
+     var $bgobj = $(this); // assigning the object
+                    
+      $(window).scroll(function() {
+                    
+    // Scroll the background at var speed
+    // the yPos is a negative value because we're scrolling it UP!                
+    var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+    
+    // Put together our final background position
+    var coords = '50% '+ yPos + 'px';
 
+    // Move the background
+    $bgobj.css({ backgroundPosition: coords });
+    
+}); // window scroll Ends
+
+ });  
+
+}); 
 
 
 
